716a00595e925567db26dd714e3a1af5ec4ecd80
Michael Schennum <mikeschenphoto@gmail.com>
2016-04-01 16:53:26 -0700

    adds one to many model relationship

--- app/models/answer.js
+++ app/models/answer.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr(),
-  detail: DS.attr()
+  detail: DS.attr(),
+  question: DS.belongsTo('question', {async: true})
 });
