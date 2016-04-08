f7465eae15b61a570a94fc1e68a9b272d350212f
Michael Schennum <mikeschenphoto@gmail.com>
2016-04-08 10:46:22 -0700

    adds date to answers

--- app/models/answer.js
+++ app/models/answer.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr(),
   detail: DS.attr(),
-  question: DS.belongsTo('question', {async: true})
+  question: DS.belongsTo('question', {async: true}),
+  date: DS.attr('date', {defaultValue() { return new Date(), }})
 });
